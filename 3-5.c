719ee4f94a055e6f6660f2e569aea4ebab2b1771
方可寻 <1505840833@qq.com>
2021-06-22 14:10:04 +0800

    20210622

--- 3-5.c
+++ 3-5.c
@@ -2,7 +2,7 @@
 int main(){
 	int c,q =1;
 	while((c= getchar()) !=EOF){
-		if(c == ""){printf("%s",q ? "``":"''");q = !q;}
+		if(c == '"'){printf("%s",q ? "``":"''");q = !q;}
 		else printf("%c",c);
 	}
 	return 0;
