1ea005737ec9ff8a56f837633f7f13bfeedc56f5
方可寻 <1505840833@qq.com>
2021-07-01 18:17:45 +0800

    20210701

--- UVa10340.c
+++ UVa10340.c
@@ -1,7 +1,33 @@
 #include<stdio.h>
 #include<string.h>
+#define maxn 10050
+char s[maxn],t[maxn];
 
 int main(){
+    int slen,tlen,tflag,sflag;
+    int f = 0;
+    while(scanf("%s",s) != EOF){
+        scanf("%s",t);
+        slen = strlen(s);
+        tlen = strlen(t);
 
+        sflag = 0;
+        for(tflag = 0;tflag < tlen;tflag++){
+            if(s[sflag] == t[tflag]){
+                sflag++;
+                if(sflag == slen){
+                    f = 1;
+                    break;
+                }
+            }
+        }
+        if(f == 1){
+            printf("YES!\n");
+        }
+        else{
+            printf("NO!\n");
+        }
+
+    } 
     return 0;
 }
--- UVa202.c
+++ UVa202.c
@@ -1,7 +1,30 @@
 #include<stdio.h>
 #include<string.h>
+#define maxn 10050
+int num[maxn];
 
 int main(){
-
+    int a,b,i,j;
+    memset(num,0,sizeof(num));
+    while(scanf("%d %d",&a,&b) != EOF){
+        int flag[maxn] = {0};
+        printf("%d / %d = %d.", a,b,a/b);
+        a = a%b*10;
+        for(i = 1;!flag[a] ;i++){
+            flag[a] = i;
+            num[i] = a/b;
+            a = a%b*10;
+        }
+        for(j = 1;j<i;j++){
+            if(j == flag[a]) printf("(");
+            if(j == 51){
+                printf("...");
+                break;
+            }
+            printf("%d",num[j]);
+        }
+        printf(")\n");
+        printf("%d = number of digits in repeating cycle",i-flag[a]);
+    }
     return 0;
 }
