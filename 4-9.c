e318874fc6daf5d1b0b2833121998a3c5f3eec9d
方可寻 <1505840833@qq.com>
2021-07-05 14:06:30 +0800

    20210705

---
+++ 4-9.c
@@ -0,0 +1,15 @@
+int sum(int* begin,int* end){
+	int n = end-begin;
+	int ans =0;
+	for(int i =0;i<n;i++)
+		ans +=begin[i];
+	return ans;
+}
+
+int sum(int* begin,int* end){
+	int *p = begin;
+	int ans = 0;
+	for(int *p = begin;p !=end ;p++)
+		ans += *p;
+	return ans;
+}
